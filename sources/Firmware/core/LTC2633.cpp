13b29b9ad49e212b64459b0165b427cc52d9a63d
Sandeepan Sengupta <sengupta.sandeepan@gmail.com>
2019-01-17 00:14:58 +0530

    Add files via upload

--- sources/Firmware/core/LTC2633.cpp
+++ sources/Firmware/core/LTC2633.cpp
@@ -1,7 +1,7 @@
 /*
     @file     LTC2633.cpp
-    @author   Tamojit Saha(github.com/TamojitSaha),
-              Sandeepan Sengupta (sandeepan.info)
+    @author   Sandeepan Sengupta (http://sandeepan.info),
+              Tamojit Saha(http://tamojitsaha.info)
 
     Arduino library part file for miniDAC module
     Designed for Linear Technology LTC®2633
@@ -11,10 +11,10 @@
     ************************************************
     The LTC®2633 is a family of dual 12-, 10-, and 8-bit voltage-output DACs with an integrated, high accuracy, low drift reference in an 8-lead TSOT-23 package.
 
-    These DACs communicate via a 2-wire I2C-compatible serial interface. The LTC2633 operates in both the standard mode (clock rate of 100kHz) and the fast mode (clock rate of 400kHz).
+    These DACs communicate via a 2-wire I2C-compatible serial interface. The LTC®2633 operates in both the standard mode (clock rate of 100kHz) and the fast mode (clock rate of 400kHz).
     *************************************************
 
-    Version: 0.1.0
+    Version: 1.0.0
     Released under CC-BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
     All text above, must be included in any redistribution
 */
@@ -35,116 +35,89 @@ LTC2633::LTC2633()
 
 }
 
-void LTC2633::link(uint8_t address)
+void LTC2633::configure(configuration _cfg)
 {
   /*
-    Takes 8bits of I2C address as input
-  */
-  if ((address == 0x10) || (address == 0x11) || (address == 0x12) || (address == 0x73))
-  {
-	    adrs = address;
-  }
-  else
-  {
-	  adrs = 0x73;
-  }
-}
-
-void LTC2633::slow()
-{
-  /*
-	Set I2C frequency to 100KHz
+    Takes 8bits of I2C Address (Data Type: "address")
+    and
+    DAC resolution in bit(s) (Data Type: "resolution")
+    and
+    I2C transfer rate as input(s) (Data Type: "rate")
   */
   Wire.begin();
-  Wire.setClock(0x0186A0);
-  Wire.begin();
+  Wire.setClock(_cfg.Rate);
+  Wire.end();
+  adrs = _cfg.Address;
+  rsln = _cfg.Resolution;
 }
 
-void LTC2633::fast()
+void LTC2633::setRate(rate _rte)
 {
   /*
-	Set I2C frequency to 400KHz
+    Sets I2C transfer rate (clock frequency)
   */
   Wire.begin();
-  Wire.setClock(0x061A80);
-  Wire.begin();
+  Wire.setClock(_rte);
+  Wire.end();
 }
 
-void LTC2633::volt(uint64_t data, uint8_t dac)
+void LTC2633::setResolution(resolution _rsl)
 {
   /*
-     Stores data to DAC register of LTC2633
-     Takes 64bit unsigned integer (unsigned long long int) and 8bits (byte) of DAC register address
-     Manipulates the data using dataman() and uses writeWire() to transmit it using I2C bus
+    Sets DAC resolution
+  */
+  rsln = _rsl;
+}
+
+void LTC2633::volt(uint64_t data, DAC dac)
+{
+  /*
+     Stores data to DAC register of LTC®2633
+     Takes 64bit unsigned integer (unsigned long long int) and "DAC" (8bits of DAC register address) as input
+     Manipulates the data using dataman() and uses wireman() to transmit it using I2C bus
   */
   uint8_t code = (write_update_dac << 4) | dac;
   dataman(data);
-  writeWire(code, byte(data_high), byte(data_low));
+  wireman(code, byte(data_high), byte(data_low));
 }
 
-void LTC2633::load(uint8_t dac)
+void LTC2633::load(DAC dac)
 {
   /*
-     Instructs LTC2633 to transfer its input register data to DAC register address
-     Takes 8bits of DAC register address (byte) as input
+     Instructs LTC®2633 to transfer its input register data to DAC register address
+     Takes "DAC" (8bits of DAC register address) as input
      Uses control() to transmit the instruction using I2C bus
   */
   uint8_t code = (write_input_reg << 4) | dac;
   control(code);
 }
 
-void LTC2633::store(uint64_t data, uint8_t dac)
+void LTC2633::store(uint64_t data, DAC dac)
 {
   /*
-     Stores data to input register of LTC2633
-     Takes 64bit unsigned integer (unsigned long long int) and 8bits (byte) of DAC register address
-     Manipulates the data using dataman() and uses writeWire() to transmit it using I2C bus
+     Stores data to input register of LTC®2633
+     Takes 64bit unsigned integer (unsigned long long int) and "DAC" (8bits of DAC register address) as input
+     Manipulates the data using dataman() and uses wireman() to transmit it using I2C bus
   */
   dataman(data);
-  writeWire(dac, data_high, data_low); /*Actually it should be -> writeWire(((write_input_reg << 4) | dac), data_high, data_low)*/
+  wireman(dac, data_high, data_low); /*Actually it should be -> wireman(((write_input_reg << 4) | dac), data_high, data_low)*/
 }
-
-
-void LTC2633::x08(void)
-{
-  /*
-	Defines resolution as 8bits
-  */
-  rsln = 0x08;
-}
-
-void LTC2633::x0A(void)
-{
-  /*
-	Defines resolution as 10bits
-  */
-  rsln = 0x0A;
-}
-
-void LTC2633::x0C(void)
-{
-  /*
-	Defines resolution as 12bits
-  */
-  rsln = 0x0C;
-}
-
 
 void LTC2633::powerOff(void)
 {
   /*
-     Instructs LTC2633 to power off completely
+     Instructs LTC®2633 to power off completely
      Uses control() to transmit the instruction using I2C bus
   */
   uint8_t code = power_down_all;
   control(code);
 }
 
-void LTC2633::powerDown(uint8_t dac)
+void LTC2633::powerDown(DAC dac)
 {
   /*
-     Instructs LTC2633 to power down its individual DAC
-     Takes DAC 8bits of register address (byte) as input
+     Instructs LTC®2633 to power down its individual DAC
+     Takes "DAC" (8bits of DAC register address) as input
      Uses control() to transmit the instruction using I2C bus
   */
   uint8_t code = (power_down_dac << 4) | dac;
@@ -154,18 +127,18 @@ void LTC2633::powerDown(uint8_t dac)
 void LTC2633::internalReference(void)
 {
   /*
-     When called, it instructs LTC2633 to switch to internal reference mode and allows user to use the DAC with internal reference voltage
+     When called, it instructs LTC®2633 to switch to internal reference mode and allows user to use the DAC with internal reference voltage
   */
-  uint8_t code = (internal_reference << 4) | BOTH;
+  uint8_t code = (internal_reference << 4) | 0x0f;
   control(code);
 }
 
 void LTC2633::externalReference(void)
 {
   /*
-     When called, it instructs LTC2633 to switch to external reference mode and allows user to use the DAC with external reference voltage
+     When called, it instructs LTC®2633 to switch to external reference mode and allows user to use the DAC with external reference voltage
   */
-  uint8_t code = (external_reference << 4) | BOTH;
+  uint8_t code = (external_reference << 4) | 0x0f;
   control(code);
 }
 
@@ -174,24 +147,25 @@ void LTC2633::control(uint8_t code)
   /*
      DECLARED IN PRIVATE : RESTRICTED ACCESS !!!
      Takes 8bits of number representing instruction (byte)
-     Transfers it to LTC2633 over I2C but using WIRE_WRITE
+     Transfers it to LTC®2633 over I2C but using WIRE_WRITE
   */
-  Wire.beginTransmission(adrs);
+  Wire.begin(adrs);
   WIRE_WRITE(code);
-  Wire.endTransmission();
+  Wire.end();
 }
-void LTC2633::writeWire(uint8_t code, uint8_t data_high, uint8_t data_low)
+
+void LTC2633::wireman(uint8_t code, uint8_t data_high, uint8_t data_low)
 {
   /*
      DECLARED IN PRIVATE : RESTRICTED ACCESS !!!
      Takes three 8bits of number representing instruction (byte), higher data bits and lower data bits
-     Transfers them to LTC2633 over I2C but using WIRE_WRITE in three packets
+     Transfers them to LTC®2633 over I2C but using WIRE_WRITE in three packets
   */
-  Wire.beginTransmission(adrs);
+  Wire.begin(adrs);
   WIRE_WRITE(code);
   WIRE_WRITE(data_high);
   WIRE_WRITE(data_low);
-  Wire.endTransmission();
+  Wire.end();
 }
 
 void LTC2633::dataman(uint64_t data)
--- sources/Firmware/core/LTC2633.h
+++ sources/Firmware/core/LTC2633.h
@@ -1,7 +1,7 @@
 /*
-    @file     LTC2633.h
-    @author   Tamojit Saha(github.com/TamojitSaha),
-              Sandeepan Sengupta (sandeepan.info)
+    @file     LTC®2633.h
+    @author   Sandeepan Sengupta (http://sandeepan.info),
+              Tamojit Saha(http://tamojitsaha.info)
 
     Arduino library part file for miniDAC module
     Designed for Linear Technology LTC®2633
@@ -11,16 +11,19 @@
     ************************************************
     The LTC®2633 is a family of dual 12-, 10-, and 8-bit voltage-output DACs with an integrated, high accuracy, low drift reference in an 8-lead TSOT-23 package.
 
-    These DACs communicate via a 2-wire I2C-compatible serial interface. The LTC2633 operates in both the standard mode (clock rate of 100kHz) and the fast mode (clock rate of 400kHz).
+    These DACs communicate via a 2-wire I2C-compatible serial interface. The LTC®2633 operates in both the standard mode (clock rate of 100kHz) and the fast mode (clock rate of 400kHz).
     *************************************************
 
-    Version: 0.1.0
+    Version: 1.0.0
     Released under CC-BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
     All text above, must be included in any redistribution
 */
 
 #ifndef LTC2633_H
 #define LTC2633_H
+
+#include "LTC2633CFG.h"
+#include <Wire.h>
 
 #if ARDUINO >= 100
 #include "Arduino.h"
@@ -31,77 +34,49 @@
 #endif
 
 
-/*LTC2633 Address Configuration*/
-#define GND    				0x10
-#define gnd    				0x10
-#define NC     				0x11
-#define nc     				0x11
-#define VCC    				0x12
-#define vcc    				0x12
-#define ALL 				0x73
-#define all 				0x73
-#define GLOBAL 				0x73
-#define global 				0x73
-/*
-#define CUSTOM 				0xZZ																		//Reserved for custom address
-#define custom 				0xZZ																		//Reserved for custom address
-*/
-
 /*Command Codes*/
-#define write_input_reg     0x00  																		//Write to Input Register n
-#define update_dac_reg      0x01  																		//Update (Power-Up) DAC Register n
-#define write_update_all    0x02  																		//Write to Input Register n, Update (Power-Up) All
-#define write_update_dac    0x03  																		//Write to and Update (Power-Up) DAC Register n
-#define power_down_dac      0x04  																		//Power-Down n
-#define power_down_all      0x05  																		//Power-Down Chip (All DAC’s and Reference)
-#define internal_reference  0x06  																		//Select Internal Reference (Power-Up Reference)
-#define external_reference  0x07  																		//Select External Reference (Power-Down Internal Reference)
-#define no_operation        0x0F  																		//No Operation
+#define write_input_reg     0x00  																        //Write to Input Register n
+#define update_dac_reg      0x01  																        //Update (Power-Up) DAC Register n
+#define write_update_all    0x02  																        //Write to Input Register n, Update (Power-Up) All
+#define write_update_dac    0x03  																        //Write to and Update (Power-Up) DAC Register n
+#define power_down_dac      0x04  																        //Power-Down n
+#define power_down_all      0x05  																        //Power-Down Chip (All DAC’s and Reference)
+#define internal_reference  0x06  																        //Select Internal Reference (Power-Up Reference)
+#define external_reference  0x07  																        //Select External Reference (Power-Down Internal Reference)
+#define no_operation        0x0F  																        //No Operation
 
-/*Address Codes*/
-#define DAC0                0x00  																		//Select DAC A
-#define dac0                0x00
-#define DAC1                0x01  																		//Select DAC B
-#define dac1                0x01
-#define BOTH                0x0F  																		//Select both the DACs
-#define both                0x0f
-
-#include<Wire.h>
 
 class LTC2633
 {
   public:
     LTC2633();
 
-	void link(uint8_t _address);																	//Set I2C Address;
+    void configure(configuration _configuration = {slow, global, XII});   //Configure DAC over I2C Address
+    void setResolution(resolution = TWELVE);                              //Switch DAC resolution
+    void setRate(rate = slow);																	          //Set I2C frequency;
 
-    void slow(void);																					//Set I2C frequency to 100KHz;
-    void fast(void);																					//Set I2C frequency to 400KHz;
 
-    void volt(uint64_t _data, uint8_t _dac = 0x0F);                                          			//Write voltage code and update BOTH DACs, by default
-    void load(uint8_t _dac = 0x0F);                                                         			//Updates previously stored value of both DAC registers
-    void store(uint64_t _data, uint8_t _dac = 0x0F);                                          			//Stores data to both DAC registers, by default
+    void internalReference(void);                                         //Switch to internal reference voltage
+    void externalReference(void);                                         //Switch to external reference voltage
 
-    void x08(void);			                                                             		//Switch to 8bits
-    void x0A(void);			                                                             		//Switch to 10bits
-    void x0C(void);			                                                             		//Switch to 12bits
 
-    void powerOff(void);                                                                      			//Shutdown entirely
-    void powerDown(uint8_t _dac = 0x0F);                                                      			//Power down all DAC, by default
+    void powerOff(void);                                                  //Shutdown entirely
+    void powerDown(DAC _dac = both);                                      //Power down all DAC, by default
+    void volt(uint64_t _data, DAC _dac = both);                           //Write voltage code and update BOTH DACs, by default
+    void load(DAC _dac = both);                                           //Updates previously stored value of both DAC registers
+    void store(uint64_t _data, DAC _dac = both);                          //Stores data to both DAC registers, by default
 
-    void internalReference(void);                                                             			//Switch to internal reference voltage
-    void externalReference(void);                                                             			//Switch to external reference voltage
 
   private:
-    uint8_t rsln = 0x0C;
-    uint8_t adrs = 0x73;
-	
+    uint8_t rsln = 0x0C;                                                  //Default resolution is set to TWELVE
+    uint8_t adrs = 0x73;                                                  //Default address is set to GLOBAL
+
     uint8_t data_low = 0x00;
     uint8_t data_high = 0x00;
 
-	void dataman(uint64_t _data);
     void control(uint8_t _code);
-    void writeWire(uint8_t _code, uint8_t _data_high, uint8_t _data_low);
+    void dataman(uint64_t _data);
+    void wireman(uint8_t _code, uint8_t _data_high, uint8_t _data_low);
 };
 
 
